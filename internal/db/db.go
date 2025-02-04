1712c1c104625c06009986e2bc8581365fe86fbd
3keyroman <46850604+3keyroman@users.noreply.github.com>
2025-02-04 13:52:55 +0100

    Update database connection to support PgBouncer (#7)
    - remove `search_path` from the connection string and DNS
    - set `search_path` explicitly as SQL statement

--- internal/db/db.go
+++ internal/db/db.go
@@ -6,20 +6,21 @@ import (
 	"errors"
 	"fmt"
 	"github.com/golang-migrate/migrate/v4"
+	"go.uber.org/zap"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	glogger "gorm.io/gorm/logger"
 	"gorm.io/gorm/schema"
 
-	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	migratepostgres "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 var log = logger.Get()
 
 func ConnectDB(config config.Config) (db *gorm.DB, err error) {
-	dsn := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=%s search_path=%s %s", config.Database.Username, config.Database.Password, config.Database.Host, config.Database.Port, config.Database.Name, config.Database.SslMode, config.Database.Schema, config.Database.Props)
+	dsn := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=%s %s", config.Database.Username, config.Database.Password, config.Database.Host, config.Database.Port, config.Database.Name, config.Database.SslMode, config.Database.Props)
 	db, err = gorm.Open(postgres.New(postgres.Config{
 		DSN:                  dsn,
 		PreferSimpleProtocol: true,
@@ -33,22 +34,42 @@ func ConnectDB(config config.Config) (db *gorm.DB, err error) {
 	return
 }
 
-func MigrateDB(config config.Config) {
+func MigrateDB(config config.Config, db *gorm.DB) {
 	log.Info("Migrating database")
-	connectionString := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?search_path=%s&x-migrations-table=%s_migrations&sslmode=%s", config.Database.Username, config.Database.Password, config.Database.Host, config.Database.Port, config.Database.Name, config.Database.Schema, config.Database.Schema, config.Database.SslMode)
-	// log.Info("Connection string: " + connectionString)
-	m, err := migrate.New(
+
+	// Convert *gorm.DB to *sql.DB
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Error("Unable to get *sql.DB from GORM", zap.Error(err))
+		return
+	}
+
+	// Wrap the *sql.DB with the postgres driver for golang-migrate
+	dbSchema := config.Database.Schema
+	driver, err := migratepostgres.WithInstance(sqlDB, &migratepostgres.Config{
+		MigrationsTable: fmt.Sprintf("%s_migrations", dbSchema),
+	})
+	if err != nil {
+		log.Error("Failed to create postgres driver for migrations", zap.Error(err))
+		return
+	}
+
+	// Create a new Migrate instance using the opened DB
+	m, err := migrate.NewWithDatabaseInstance(
 		"file://migrations",
-		connectionString,
+		config.Database.Name,
+		driver,
 	)
 	if err != nil {
-		log.Error(err.Error())
+		log.Error("Failed to create migrate instance", zap.Error(err))
+		return
 	}
+
+	// Run the migrations
 	if err := m.Up(); err != nil {
+		// Only log if it's something other than "no change"
 		if !errors.Is(err, migrate.ErrNoChange) {
-			{
-				log.Error(err.Error())
-			}
+			log.Error("Migration failed", zap.Error(err))
 		}
 	}
 }
